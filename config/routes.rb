5f8d2c859a6ede82d051429ee02c4a67b5f3e08e
Marcelo Alarcón <marceloalarconbarrenechea@gmail.com>
2023-05-26 12:22:30 -0400

    Add API end points for posts and comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,25 +2,17 @@
 
 Rails.application.routes.draw do
   devise_for :users
-  root 'users#index'
 
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create, :destroy] do
-      resources :comments, only: [:new, :create, :destroy]
-      resources :likes, only: [:create]
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :show] do
+        resources :posts, only: [:index, :show, :new, :create, :destroy] do
+          resources :comments, only: [:index, :new, :create, :destroy]
+          resources :likes, only: [:create]
+        end
+      end
     end
   end
-
-  delete '/logout', to: 'users#destroy', as: :logout
-
-  get 'post_create_new', to: 'posts#new'
-  post 'post_create_new', to: 'posts#create'
-
-  get 'comment_create_new', to: 'comments#new'
-  post 'comment_create_new', to: 'comments#create'
-
-  post 'like_create_new', to: 'likes#create'
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
